aabb7baac1562890ebb632be172c07c9452b7ab8
andriy <andrey.xf@gmail.com>
2025-07-17 22:28:01 -0700

    made String(randomLength:) public

--- src/Extensions/String+random.swift
+++ src/Extensions/String+random.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 extension String {
-	init(randomLength length: Int) {
+	public init(randomLength length: Int) {
 		let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 		self = String((0..<length).map { _ in letters.randomElement()! })
 	}
